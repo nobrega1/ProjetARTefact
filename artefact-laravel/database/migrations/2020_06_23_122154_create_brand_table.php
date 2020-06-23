0cee8c412b880ba7c5972a165d3c87481eba38e5
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-06-23 14:26:54 +0200

    migration company & brand

---
+++ artefact-laravel/database/migrations/2020_06_23_122154_create_brand_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->id('no');
+            $table->foreignId('companyno')->references('no')->on('company');
+            $table->string('name');
+            $table->string('shortdescr');
+            $table->text('longdescr');
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+}
