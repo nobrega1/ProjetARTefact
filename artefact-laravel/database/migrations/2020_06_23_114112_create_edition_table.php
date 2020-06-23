77aaf6c39eb54ccfacfbd42344d599c1a5bcdba2
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-06-23 13:46:20 +0200

    created table edition

---
+++ artefact-laravel/database/migrations/2020_06_23_114112_create_edition_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEditionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('edition', function (Blueprint $table) {
+            $table->id('No');
+            $table->foreignId('event_No')->references('No')->on('event');
+            $table->string('Place');
+            $table->date('Start');
+            $table->date('End');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('edition');
+    }
+}
