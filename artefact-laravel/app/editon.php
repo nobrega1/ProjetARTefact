cbe7ef527a73cc8d37c3d76ee9d6bbfda001a10e
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-06-24 10:45:43 +0200

    linked edition and event together

--- artefact-laravel/app/editon.php
+++ artefact-laravel/app/editon.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class editon extends Model
 {
+    protected $fillable = ['place','start','end'];
+
     public function event()
     {
        return $this->belongsTo('App\event');
--- artefact-laravel/app/event.php
+++ artefact-laravel/app/event.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class event extends Model
 {
+    protected $fillable=['name'];
+
     public function editions()
      {
         return $this->hasMany('App\Edition');
