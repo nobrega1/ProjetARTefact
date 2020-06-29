1a3618befb60e7e520bc749c4b612d416a331287
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-06-29 13:24:02 +0200

    backbone debut

--- artefact-laravel/src/index.js
+++ artefact-laravel/src/index.js
@@ -1,1 +1,4 @@
-console.log("test")
+import Catalogue from 'entities/catalogue/collection'
+
+let catalogue = new Catalogue();
+catalogue.fetch();
---
+++ artefact-laravel/src/lib/ImBackbone.js
@@ -0,0 +1,13 @@
+import Backbone from 'Backbone';
+
+export class ImModel extends Backbone.Model {
+
+}
+
+export class ImView extends Backbone.View {
+
+}
+
+export class ImCollection extends Backbone.Collection {
+
+}
