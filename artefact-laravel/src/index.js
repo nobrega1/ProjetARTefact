b36d52fcdf908fcb7a8f1cf97374376c74b59ee6
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-06-30 10:57:29 +0200

    tabula rasa

--- artefact-laravel/src/index.js
+++ artefact-laravel/src/index.js
@@ -1,5 +1,15 @@
-import Catalogue from '../resources/js/Viewcatalogue'
+/* import Catalogue from '../resources/js/Viewcatalogue'
+ */
 
 
 
+let catalogues = new catalogue();
+let vCatalogue = new ViewCatalogue({
+    collection: catalogues,
+    el: '#'
+});
 
+catalogues.fetch({reset:true});
+setInterval(() => {  
+    tasks.fetch();
+  }, 10000);
